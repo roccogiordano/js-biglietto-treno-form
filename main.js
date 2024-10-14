ca3bd3bf114a83b8c9ae987fc8f63a8798f4a25b
roccogiordano <rocco.giordano573@gmail.com>
2024-10-14 04:17:55 +0200

    feat: initializated variables

---
+++ main.js
@@ -0,0 +1,15 @@
+// Variables
+
+const kmPrice = 0.21;
+
+let price;
+
+const inputKmSection = document.getElementById("inputKm");
+
+const inputAgeSection = document.getElementById("inputAge");
+
+let inputKmValue;
+
+let inputAgeValue;
+
+//
